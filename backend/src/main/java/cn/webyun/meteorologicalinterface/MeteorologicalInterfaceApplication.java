640966594057a4e8a71a03018dfd3ce9176a3032
wasabi18001029521 <yuxf@webyun.cn>
2019-03-11 14:51:23 +0800

    init pro

---
+++ backend/src/main/java/cn/webyun/meteorologicalinterface/MeteorologicalInterfaceApplication.java
@@ -0,0 +1,13 @@
+package cn.webyun.meteorologicalinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteorologicalInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeteorologicalInterfaceApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/cn/webyun/meteorologicalinterface/MeteorologicalInterfaceApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.webyun.meteorologicalinterface;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeteorologicalInterfaceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
