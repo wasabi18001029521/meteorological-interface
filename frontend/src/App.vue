dda30b8309644c3a68c4d4b757f7479eca53bf8b
webyun123 <zhaoxj@webyun.cn>
2019-03-11 15:19:38 +0800

    add header footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
+body{
+margin: 0;
+padding: 0;
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,5 +24,6 @@
       color: #42b983;
     }
   }
+}
 }
 </style>
---
+++ frontend/src/views/register.vue
@@ -0,0 +1,16 @@
+<template>
+ <el-main>注册账号</el-main>
+</template>
+
+<script>
+import{Container,Header,Main,Footer} from 'element-ui'
+export default {
+  name: 'register',
+  components: {
+
+  }
+}
+</script>
+<style lang="less">
+
+</style>
