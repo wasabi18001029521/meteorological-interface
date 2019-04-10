c913612978550ccdc06d21a1bb9d7898b3183515
webyun123 <zhaoxj@webyun.cn>
2019-04-10 17:48:50 +0800

    api开发文档页面修改

--- frontend/src/views/documents/index.vue
+++ frontend/src/views/documents/index.vue
@@ -1,105 +1,104 @@
 <template>
-    <el-container style="height: 500px; border: 1px solid #eee">
-        <el-aside width="400px" style="background-color: rgb(238, 241, 246)">
-            <el-menu :default-openeds="['1', '3']">
-                <el-submenu index="1">
-                    <template slot="title"><i class="el-icon-message"></i>API开发文档</template>
-                    <el-submenu index="1-4">
-                        <template slot="title">中国地面自动站</template>
-                        <el-menu-item index="1-4-1" @click="autoarea">中国地面自动站区域查询数据获取接口</el-menu-item>
-                        <el-menu-item index="1-4-2" @click="autoone">中国地面自动站单站查询数据获取接口</el-menu-item>
-                        <el-menu-item index="1-4-3" @click="autonearest">中国地面自动站最近站数据获取接口</el-menu-item>
+    <el-container class="u-documents-box">
+     <el-main>
+        <el-row>
+          <el-col :span="7">
+            <el-aside class="u-document-nav" width="360px" style="background-color: rgb(238, 241, 246)">
+                <el-menu :default-openeds="['1', '3']">
+                    <el-submenu index="1">
+                        <template slot="title"><i class="el-icon-message"></i>API开发文档</template>
+                        <el-submenu index="1-4">
+                            <template slot="title">中国地面自动站</template>
+                            <el-menu-item index="1-4-1" @click="changeAPI('autoArea')">中国地面自动站区域查询数据获取接口</el-menu-item>
+                            <el-menu-item index="1-4-2" @click="changeAPI('autoOne')">中国地面自动站单站查询数据获取接口</el-menu-item>
+                            <el-menu-item index="1-4-3" @click="changeAPI('autoNearest')">中国地面自动站最近站数据获取接口</el-menu-item>
+                        </el-submenu>
+                        <el-submenu index="2-4">
+                            <template slot="title">精细化城市预报</template>
+                            <el-menu-item index="2-4-1" @click="changeAPI('cityArea')">精细化城市预报矩形区域查询数据获取接口</el-menu-item>
+                            <el-menu-item index="2-4-2" @click="changeAPI('cityOne')">精细化城市预报单站查询数据获取接口</el-menu-item>
+                        </el-submenu>
                     </el-submenu>
-                    <el-submenu index="2-4">
-                        <template slot="title">精细化城市预报</template>
-                        <el-menu-item index="2-4-1" @click="cityarea">精细化城市预报矩形区域查询数据获取接口</el-menu-item>
-                        <el-menu-item index="2-4-2" @click="cityone">精细化城市预报单站查询数据获取接口</el-menu-item>
+                    <el-submenu index="2">
+                        <template slot="title"><i class="el-icon-menu"></i>账号相关问题</template>
+                        <el-menu-item-group>
+                            <template slot="title">分组一</template>
+                            <el-menu-item index="2-1">选项1</el-menu-item>
+                            <el-menu-item index="2-2">选项2</el-menu-item>
+                        </el-menu-item-group>
+                        <el-menu-item-group title="分组2">
+                            <el-menu-item index="2-3">选项3</el-menu-item>
+                        </el-menu-item-group>
+                        <el-submenu index="2-4">
+                            <template slot="title">选项4</template>
+                            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+                        </el-submenu>
                     </el-submenu>
-                </el-submenu>
-                <el-submenu index="2">
-                    <template slot="title"><i class="el-icon-menu"></i>账号相关问题</template>
-                    <el-menu-item-group>
-                        <template slot="title">分组一</template>
-                        <el-menu-item index="2-1">选项1</el-menu-item>
-                        <el-menu-item index="2-2">选项2</el-menu-item>
-                    </el-menu-item-group>
-                    <el-menu-item-group title="分组2">
-                        <el-menu-item index="2-3">选项3</el-menu-item>
-                    </el-menu-item-group>
-                    <el-submenu index="2-4">
-                        <template slot="title">选项4</template>
-                        <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+                    <el-submenu index="3">
+                        <template slot="title"><i class="el-icon-setting"></i>产品相关问题</template>
+                        <el-menu-item-group>
+                            <template slot="title">分组一</template>
+                            <el-menu-item index="3-1">选项1</el-menu-item>
+                            <el-menu-item index="3-2">选项2</el-menu-item>
+                        </el-menu-item-group>
+                        <el-menu-item-group title="分组2">
+                            <el-menu-item index="3-3">选项3</el-menu-item>
+                        </el-menu-item-group>
+                        <el-submenu index="3-4">
+                            <template slot="title">选项4</template>
+                            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+                        </el-submenu>
                     </el-submenu>
-                </el-submenu>
-                <el-submenu index="3">
-                    <template slot="title"><i class="el-icon-setting"></i>产品相关问题</template>
-                    <el-menu-item-group>
-                        <template slot="title">分组一</template>
-                        <el-menu-item index="3-1">选项1</el-menu-item>
-                        <el-menu-item index="3-2">选项2</el-menu-item>
-                    </el-menu-item-group>
-                    <el-menu-item-group title="分组2">
-                        <el-menu-item index="3-3">选项3</el-menu-item>
-                    </el-menu-item-group>
-                    <el-submenu index="3-4">
-                        <template slot="title">选项4</template>
-                        <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-                    </el-submenu>
-                </el-submenu>
-            </el-menu>
-        </el-aside>
-        <el-col>
-            <div v-if="loginType === 'autoarea'">
-                <div>中国地面自动站区域查询数据获取接口</div>
-                <div>----------------------------------------------------------</div>
-                <div>GET /station/auto/area/</div>
-                <br/>
-                <div>根据输入的矩形区域范围，查找区域内全部的站点在某一时刻的中国地面气象站逐小时观测数据，并提供分级抽希功能。</div>
-                <br/>
-                <div>请求地址试例</div>
-                <div>矩形区域范围内,中国地面气象站逐小时观测数据  https://localhost:8080/station/auto/area?key=your_api_key&level=1&maxLon=22.22&maxLat=22.22
-                    &minLon=22.22&minLat=22.22&var=[1,2]&datatime=2019-04-03 10:13:55&format=2019-04-09 02:01:58
+                </el-menu>
+            </el-aside>
+          </el-col>
+          <el-col :span="15">
+            <div class="u-document-con">
+                <div class="u-title u-size16">
+                    <span  v-if="apiType === 'autoArea'">中国地面自动站区域查询数据获取接口</span>
+                    <span  v-if="apiType === 'autoOne'">中国地面自动站单站查询数据获取接口</span>
+                    <span  v-if="apiType === 'autoNearest'">中国地面自动站最近站数据获取接口</span>
+                    <span  v-if="apiType === 'cityArea'">精细化城市预报矩形区域查询数据获取接口</span>
+                    <span  v-if="apiType === 'cityOne'">精细化城市预报单站查询数据获取接口</span>
+                    <span class="u-line"></span>
                 </div>
-                <div>参数</div>
-                <div>
-                    <el-table
-                            :data="tableDataautoarea"
-                            style="width: 100%">
-                        <el-table-column
-                                prop="name"
-                                label="参数名"
-                                width="180">
-                        </el-table-column>
-                        <el-table-column
-                                prop="type"
-                                label="类型"
-                                width="180">
-                        </el-table-column>
-                        <el-table-column
-                                prop="must"
-                                label="必须"
-                                width="180">
-                        </el-table-column>
-                        <el-table-column
-                                prop="remarks"
-                                label="备注">
-                        </el-table-column>
-                    </el-table>
+                <div class="ieKsWY">
+                    <span  v-if="apiType === 'autoArea'">GET /station/auto/area/</span>
+                    <span  v-if="apiType === 'autoOne'">/life/suggestion.json</span>
+                    <span  v-if="apiType === 'autoNearest'">/life/chinese_calendar.json</span>
+                    <span  v-if="apiType === 'cityArea'">/life/driving_restriction.json</span>
+                    <span  v-if="apiType === 'cityOne'">/geo/moon.json</span>
                 </div>
-
+                <div class="u-describe">根据输入的矩形区域范围，查找区域内全部的站点在某一时刻的中国地面气象站逐小时观测数据，并提供分级抽希功能。</div>
+                <div class="u-tips">请求地址试例</div>
+                <div class="u-describe">矩形区域范围内,中国地面气象站逐小时观测数据
+                    <span class="u-orange">https://localhost:8080/station/auto/area?key=your_api_key&level=1&maxLon=22.22&maxLat=22.22 &minLon=22.22&minLat=22.22&var=[1,2]&datatime=2019-04-03 10:13:55&format=2019-04-09 02:01:58</span></div>
+                <div class="u-tips">参数</div>
+                <el-table :data="tableData" :header-cell-style="{background:'#eef1f6',color:'#606266'}" style="width: 100%">
+                    <el-table-column prop="name" label="参数名" align="center"></el-table-column>
+                    <el-table-column prop="type" label="类型" align="center"></el-table-column>
+                    <el-table-column prop="must" label="必须" align="center"></el-table-column>
+                    <el-table-column prop="remarks" label="备注" align="center"></el-table-column>
+                </el-table>
+                <div class="u-tips">返回结果</div>
+                <div class="u-result">
+                    <div><i>1</i><span>{</span></div>
+                    <div><i>2</i><span class="u-B50000">"results": </span><span>[{</span></div>
+                    <div><i>3</i><span  class="u-FE5400">"location":</span><span> {</span></div>
+                    <div><i>4</i><span  class="u-FE5400">"id": </span><span class="u-B50000">"WX4FBXXFKE4F",</span></div>
+                    <div><i>5</i><span  class="u-FE5400">"name":</span><span class="u-B50000"> "北京",</span></div>
+                    <div><i>6</i><span  class="u-FE5400"> "country":</span><span class="u-B50000"> "CN",</span></div>
+                    <div><i>7</i><span  class="u-FE5400"> "path": </span><span class="u-B50000">"北京,北京,中国",</span></div>
+                    <div><i>8</i><span  class="u-FE5400"> "timezone": </span><span class="u-B50000">"Asia/Shanghai",</span></div>
+                    <div><i>9</i><span  class="u-FE5400"> "timezone_offset":</span><span class="u-B50000"> "+08:00"</span></div>
+                    <div><i>10</i><span class="u-padding30">   },</span></div>
+                    <div><i>11</i><span class="u-padding20">   }]</span></div>
+                    <div><i>12</i><span>   }</span></div>
+                </div>
             </div>
-            <div v-if="loginType === 'autoone'">中国地面自动站单站查询数据获取接口</div>
-
-            <div v-if="loginType === 'autonearest'">中国地面自动站最近站数据获取接口</div>
-
-        <div v-if="loginType === 'cityarea'">
-            　<div>精细化城市预报矩形区域查询数据获取接口</div>
-                <div>----------------------------------------------------------</div>
-                <div>GET localhost</div>
-        </div>
-
-            <div v-if="loginType === 'cityone'">精细化城市预报单站查询数据获取接口</div>
-        </el-col>
+          </el-col>
+        </el-row>
+     </el-main>
     </el-container>
 </template>
 
@@ -108,65 +107,47 @@
         name: "autoarea",
        data(){
         return {
-            loginType:'indexImg',
-            tableDataautoarea: [{
-                name: 'level',
-                type: 'integer',
-                must: '是',
-                remarks:'抽稀级别 [1,2,3,4]'
-            }, {
-                name: 'maxLon',
-                type: 'number',
-                must: '是',
-                remarks:'最大经度'
-            }, {
-                name: 'maxLat',
-                type: 'number',
-                must: '是',
-                remarks:'最大纬度'
-            }, {
-                name: 'minLon',
-                type: 'number',
-                must: '是',
-                remarks:'最小经度'
-            }, {
-                name: 'minLat',
-                type: 'number',
-                must: '是',
-                remarks:'最小纬度'
-            }, {
-                name: 'var',
-                type: 'array',
-                must: '是',
-                remarks:'查询的字段数组'
-            }, {
-                name: 'datatime',
-                type: 'string',
-                must: '是',
-                remarks:'数据时间，格式 yyyyMMddHH，如果不传默认取最新的'
-            }, {
-                name: 'var',
-                type: 'string',
-                must: '否',
-                remarks:'时间格式，默认yyyyMMddHHmmss'
-            }]
+            apiType:'autoArea',
+            tableData:[{name: 'level', type: 'integer', must: '是', remarks:'抽稀级别 [1,2,3,4]' },
+                       {name: 'maxLon', type: 'number',must: '是',remarks:'1'},
+                       {name: 'maxLat',type: 'number', must: '是',remarks:'1'},
+                        {name: 'minLon',type: 'number',must: '是',remarks:'1'}],
+
        }
     },
         methods: {
-            autoarea(){
-                  this.loginType='autoarea'
-            },
-            autoone(){
-                this.loginType='autoone'
-            },
-            autonearest(){
-                this.loginType='autonearest'
-            },
-            cityarea(){
-                this.loginType='cityarea'
-            },
-            cityone(){
-                this.loginType='cityone'
+            changeAPI(type){
+                if(type == "autoArea"){
+                    this.apiType='autoArea'
+                    this.tableData = [{name: 'level', type: 'integer', must: '是', remarks:'抽稀级别 [1,2,3,4]' },
+                     {name: 'maxLon', type: 'number',must: '是',remarks:'1'},
+                      {name: 'maxLat',type: 'number', must: '是',remarks:'1'},
+                      {name: 'minLon',type: 'number',must: '是',remarks:'1'}];
+                }else if(type == "autoOne"){
+                    this.apiType='autoOne'
+                     this.tableData = [{name: 'level', type: 'integer', must: '是', remarks:'抽稀级别 [1,2,3,4]' },
+                     {name: 'maxLon', type: 'number',must: '是',remarks:'2'},
+                     {name: 'maxLat',type: 'number', must: '是',remarks:'2'},
+                     {name: 'minLon',type: 'number',must: '是',remarks:'2'}];
+                }else if(type == "autoNearest"){
+                    this.apiType='autoNearest'
+                    this.tableData = [{name: 'level', type: 'integer', must: '是', remarks:'抽稀级别 [1,2,3,4]' },
+                    {name: 'maxLon', type: 'number',must: '是',remarks:'3'},
+                    {name: 'maxLat',type: 'number', must: '是',remarks:'3'},
+                    {name: 'minLon',type: 'number',must: '是',remarks:'3'}];
+                }else if(type == "cityArea"){
+                    this.apiType='cityArea'
+                     this.tableData = [{name: 'level', type: 'integer', must: '是', remarks:'抽稀级别 [1,2,3,4]' },
+                     {name: 'maxLon', type: 'number',must: '是',remarks:'4'},
+                     {name: 'maxLat',type: 'number', must: '是',remarks:'4'},
+                      {name: 'minLon',type: 'number',must: '是',remarks:'4'}];
+                }else if(type == "cityOne"){
+                    this.apiType='cityOne'
+                     this.tableData = [{name: 'level', type: 'integer', must: '是', remarks:'抽稀级别 [1,2,3,4]' },
+                     {name: 'maxLon', type: 'number',must: '是',remarks:'5'},
+                     {name: 'maxLat',type: 'number', must: '是',remarks:'5'},
+                     {name: 'minLon',type: 'number',must: '是',remarks:'5'}];
+                }
             },
 
 
@@ -178,13 +159,72 @@
 </script>
 
 <style scoped>
-    .el-header {
-        background-color: #B3C0D1;
-        color: #333;
-        line-height: 60px;
-    }
-
-    .el-aside {
-        color: #333;
-    }
+.u-document-con{
+    padding: 20px;
+    background-color: #fff;
+}
+.u-title{
+    border-bottom: 1px solid #EBEBEB;
+    padding-bottom: 10px;
+    position: relative;
+}
+.u-title .u-line{
+    position: absolute;
+    font-size: 16px;
+    bottom: 0px;
+    width: 300px;
+    border-bottom: 3px solid #0193FF;
+    left: 0px;
+}
+.ieKsWY{
+    border-radius: 3px;
+    padding: 10px 15px;
+    background: rgb(217, 237, 247);
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(188, 232, 241);
+    border-image: initial;
+    margin: 30px 0px;
+}
+.u-describe{
+    color: rgb(127, 133, 148);
+    margin: 10px 0px 20px;
+}
+.u-tips{
+    font-size: 16px;
+    font-weight: 500;
+    color: rgb(59, 69, 92);
+    margin: 25px 0px 10px;
+}
+.u-orange{
+    color: rgb(250, 170, 39);
+}
+.u-result{
+    background-color: #FCFDFE;
+    border: 1px solid #E7EDF4;
+    padding: 10px;
+    color: #282E32;
+}
+.u-result>div{
+    line-height: 30px;
+}
+.u-result i{
+    margin-right: 14px;
+    color: #919CA4;
+     font-style:normal
+}
+.u-FE5400{
+    color:#FE5400;
+    padding-left: 30px;
+}
+.u-padding30{
+  padding-left: 30px;
+}
+.u-padding20{
+  padding-left: 20px;
+}
+.u-B50000{
+    color:#B50000;
+    padding-left: 10px;
+}
 </style>
